fda9ebfa03611b6391bf5483b8f795a49341525e
Jessica Miranda <jessicamirandapaez@gmail.com>
2026-05-19 02:53:35 +0000

    resolve merge conflict in models.py

--- src/api/models.py
+++ src/api/models.py
@@ -2,12 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean, Float, Text, Date
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, date
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(80), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(80), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
@@ -19,9 +22,17 @@ class User(db.Model):
     progress_logs: Mapped[list["ProgressLog"]] = relationship(back_populates="user")
     nutrition_logs: Mapped[list["NutritionLog"]] = relationship(back_populates="user")
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
@@ -30,8 +41,6 @@ class User(db.Model):
 class Profile(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"), nullable=False)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
     age: Mapped[int] = mapped_column(nullable=False)
     weight: Mapped[float] = mapped_column(Float, nullable=False)        # en kg
     height: Mapped[float] = mapped_column(Float, nullable=False)        # en cm
@@ -45,8 +54,6 @@ class Profile(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "name": self.name,
-            "last_name": self.last_name,
             "age": self.age,
             "weight": self.weight,
             "height": self.height,
