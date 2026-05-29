bc60764a345fc7bfa09ea99a0dde4dbe4064186a
Darielescobar <escobardariel5@gmail.com>
2026-05-29 23:26:33 +0000

    storage.setItem("token", data.token)

--- src/front/pages/Signup.jsx
+++ src/front/pages/Signup.jsx
@@ -65,6 +65,8 @@ export const Signup = () => {
             console.log(data);
 
             if (response.ok) {
+                sessionStorage.setItem("token", data.token);
+                sessionStorage.setItem("user", JSON.stringify(data.user));
                 alert("Account created successfully");
                 navigate("/dashboard");
             } else {
